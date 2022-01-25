a9893d4d999e5a2cf3ad957a44b01c783f897e45
bosungpark <qkrqhtjd0806@naver.com>
2022-01-25 17:22:30 +0900

    implement CRUD using APIView

--- project/views.py
+++ project/views.py
@@ -8,4 +8,48 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 
+class BlogList(APIView):
+    # show blog list
+    def get(self,request):
+        blogs=Blog.objects.all()
+
+        serializer=BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+
+    #create new post
+    def post(self, request):
+        #request.data==user's input data
+        serializer=BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BlogDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
+    #show details
+    def get(self, request, pk, format=None):
+        blog=self.get_object(pk)
+        serializer=BlogSerializer(blog)
+        return Response(serializer.data)
+    #update Blog
+    def put(self, request, pk, format=None):
+        blog=self.get_object(pk)
+        serializer=BlogSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        blog=self.get_object(pk)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
 
